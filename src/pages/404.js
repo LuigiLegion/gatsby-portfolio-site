385fbc6c5985b2006b2138d1b2304e65bc487f02
Tal Luigi <luigi.t.agr@gmail.com>
2019-10-15 19:20:43 -0400

    update: changed 404 page flavor text

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,25 +1,24 @@
-import React, { Component } from "react";
-import { graphql } from "gatsby";
-
-import Layout from "../components/layout";
-import SEO from "../components/seo";
+import { graphql } from 'gatsby'
+import React, { Component } from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 class NotFoundPage extends Component {
   render() {
-    const { data } = this.props;
-    const siteTitle = data.site.siteMetadata.title;
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <h1>Page Not Found</h1>
+        <p>This route does not exist.</p>
       </Layout>
-    );
+    )
   }
 }
 
-export default NotFoundPage;
+export default NotFoundPage
 
 export const pageQuery = graphql`
   query {
@@ -29,4 +28,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`;
+`
