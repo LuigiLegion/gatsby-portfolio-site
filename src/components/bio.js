3950ff9e6adc14ffb5dbf378cac343345f6d3592
Tal Luigi <luigi.t.agr@gmail.com>
2019-12-05 01:20:55 -0500

    chore: reordered imports across all components

--- src/components/bio.js
+++ src/components/bio.js
@@ -6,34 +6,35 @@
  */
 
 import React from 'react'
-import { useStaticQuery, graphql, Link } from 'gatsby'
-import Image from 'gatsby-image'
+import { Link } from 'gatsby'
+// import { useStaticQuery, graphql, Link } from 'gatsby'
+// import Image from 'gatsby-image'
 
 import { rhythm } from '../utils/typography'
 
 const Bio = () => {
-  const data = useStaticQuery(graphql`
-    query BioQuery {
-      avatar: file(absolutePath: { regex: "/profile-pic.png/" }) {
-        childImageSharp {
-          fixed(width: 50, height: 50) {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
-      site {
-        siteMetadata {
-          author
-          social {
-            twitter
-          }
-        }
-      }
-    }
-  `)
+  // const data = useStaticQuery(graphql`
+  //   query BioQuery {
+  //     avatar: file(absolutePath: { regex: "/profile-pic.png/" }) {
+  //       childImageSharp {
+  //         fixed(width: 50, height: 50) {
+  //           ...GatsbyImageSharpFixed
+  //         }
+  //       }
+  //     }
+  //     site {
+  //       siteMetadata {
+  //         author
+  //         social {
+  //           twitter
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
 
   // const { author, social } = data.site.siteMetadata;
-  const { author } = data.site.siteMetadata
+  // const { author } = data.site.siteMetadata
 
   return (
     <div
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,6 @@
-import { graphql } from 'gatsby'
 import React, { Component } from 'react'
+import { graphql } from 'gatsby'
+
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
