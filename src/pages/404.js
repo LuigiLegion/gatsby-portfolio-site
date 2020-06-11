d935959f430b52a0b1680e7d01cee6c86dbb058a
Tal Luigi <luigi.t.agr@gmail.com>
2020-06-11 02:00:29 -0400

    feat: refactored the NotFoundPage class component as a functional component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,29 +1,27 @@
 /* eslint-disable react/jsx-pascal-case */
 
 // Imports
-import React, { Component } from 'react';
+import React from 'react';
 import { graphql } from 'gatsby';
+import PropTypes from 'prop-types';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 // Component
-class NotFoundPage extends Component {
-  render() {
-    const { data } = this.props;
-    const siteTitle = data.site.siteMetadata.title;
+const NotFoundPage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title;
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="404: Not Found" />
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="404: Not Found" />
 
-        <h1>Page Not Found</h1>
+      <h1>Page Not Found</h1>
 
-        <p>This route does not exist.</p>
-      </Layout>
-    );
-  }
-}
+      <p>This route does not exist.</p>
+    </Layout>
+  );
+};
 
 // Query
 export const pageQuery = graphql`
@@ -35,5 +33,11 @@ export const pageQuery = graphql`
     }
   }
 `;
+
+// Prop Types
+NotFoundPage.propTypes = {
+  data: PropTypes.object,
+  location: PropTypes.object,
+};
 
 export default NotFoundPage;
