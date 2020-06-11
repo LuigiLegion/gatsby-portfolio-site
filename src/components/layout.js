981908782cbe83fdf59e2114bb9a8546d56e5765
Tal Luigi <luigi.t.agr@gmail.com>
2020-06-11 01:59:52 -0400

    feat: refactored the Layout class component as a functional component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,89 +1,94 @@
 /* eslint-disable no-undef */
 
 // Imports
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
 
 import { rhythm, scale } from '../utils/typography';
 
 // Component
-class Layout extends Component {
-  render() {
-    const { location, title, children } = this.props;
-    const rootPath = `${__PATH_PREFIX__}/`;
-    let header;
+const Layout = ({ location, title, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`;
+  let header;
 
-    if (location.pathname === rootPath) {
-      header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to="/"
-          >
-            {title}
-          </Link>
-        </h1>
-      );
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to="/"
-          >
-            {title}
-          </Link>
-        </h3>
-      );
-    }
-
-    return (
-      <div
+  if (location.pathname === rootPath) {
+    header = (
+      <h1
         style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          ...scale(1.5),
+          marginBottom: rhythm(1.5),
+          marginTop: 0,
         }}
       >
-        <header>{header}</header>
-
-        <main>{children}</main>
-
-        <footer className="flex-row-container">
-          © {new Date().getFullYear()}, Built with
-          <span style={{ color: '#eee' }}>.</span>
-          <div className="flex-medium-containee left">
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </div>
-          {location.pathname !== rootPath ? (
-            <div className="flex-containee right">
-              <Link to="/">← Back</Link>
-            </div>
-          ) : null}
-        </footer>
-      </div>
+        <Link
+          style={{
+            boxShadow: `none`,
+            textDecoration: `none`,
+            color: `inherit`,
+          }}
+          to="/"
+        >
+          {title}
+        </Link>
+      </h1>
+    );
+  } else {
+    header = (
+      <h3
+        style={{
+          fontFamily: `Montserrat, sans-serif`,
+          marginTop: 0,
+        }}
+      >
+        <Link
+          style={{
+            boxShadow: `none`,
+            textDecoration: `none`,
+            color: `inherit`,
+          }}
+          to="/"
+        >
+          {title}
+        </Link>
+      </h3>
     );
   }
-}
+
+  return (
+    <div
+      style={{
+        marginLeft: `auto`,
+        marginRight: `auto`,
+        maxWidth: rhythm(24),
+        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+      }}
+    >
+      <header>{header}</header>
+
+      <main>{children}</main>
+
+      <footer className="flex-row-container">
+        © {new Date().getFullYear()}, Built with
+        <span style={{ color: '#eee' }}>.</span>
+        <div className="flex-medium-containee left">
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </div>
+        {location.pathname !== rootPath ? (
+          <div className="flex-containee right">
+            <Link to="/">← Back</Link>
+          </div>
+        ) : null}
+      </footer>
+    </div>
+  );
+};
+
+// Prop Types
+Layout.propTypes = {
+  location: PropTypes.object,
+  title: PropTypes.string,
+  children: PropTypes.array,
+};
 
 export default Layout;
